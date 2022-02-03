e16ce12421e6d59beee0322d3a7b1695e7798956
code-victor-now <vcalesco@gmail.com>
2022-02-03 10:31:09 -0300

    Create hello_world.py
    First steps learning to Automate the Boring Stuff Python

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+print('Hello World!')
+print('What is your name?') # ask for name
+name = input()
+print('It is good to meet you, ' + name)
+print('The lenght of your name is: ')
+print(len(name))
+print('What is your age?')
+myage = input()
+print('You´ll be ' + str(int(myage) + 1) + ' in a year.')
