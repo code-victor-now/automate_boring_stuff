aed0137b69ce038e40da58fc9e66a29764b3e3f8
code-victor-now <vcalesco@gmail.com>
2022-02-03 14:11:43 -0300

    elif statement learning

---
+++ alice_age.py
@@ -0,0 +1,10 @@
+name = 'anon'
+age = 7
+if name == 'Alice':
+    print('Hi Alice!')
+elif age < 12:
+    print('You´re not Alice, kiddo!')
+elif age > 150:
+    print('Alice is not a Mathusalem!')
+elif age > 100:
+    print('Alice is not a grannie!')
